963577a101de9259e5d41430efd5ac5cdd5c2fdc
Jupneet <jupneet@Jupneets-MacBook-Air.local>
2017-08-27 01:00:37 +0530

    adding splash

--- app/src/main/java/com/jupneetsingh/noty/activities/NotySplashActivity.java
+++ app/src/main/java/com/jupneetsingh/noty/activities/NotySplashActivity.java
@@ -1,8 +1,12 @@
 package com.jupneetsingh.noty.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.jupneetsingh.noty.R;
 
 /**
  * Created by jupneet on 24/08/17.
@@ -10,10 +14,25 @@ import android.support.v7.app.AppCompatActivity;
 
 public class NotySplashActivity extends AppCompatActivity {
 
+    final Handler handler = new Handler();
+    private final int TIME_TO_STAY_ON_SPLASH_MILLIS = 2000;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
 
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                openNoteListActivity();
+            }
+        }, TIME_TO_STAY_ON_SPLASH_MILLIS);
 
+    }
+
+    private void openNoteListActivity() {
+        Intent intent = new Intent(this, NotesListActivity.class);
+        startActivity(intent);
     }
 }
